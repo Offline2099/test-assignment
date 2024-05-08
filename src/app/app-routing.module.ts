f9d216e89ab77078d4382a5dab98cd95b9d3bbac
offline2099 <offline2099@protonmail.com>
2024-05-08 19:30:55 +0700

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
+import { TicketComponent } from './components/ticket/ticket.component';
+
+const routes: Routes = [
+  { 
+    path: '', 
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  { 
+    path: 'login', 
+    component: LoginComponent
+  },
+  { 
+    path: 'profile/:id', 
+    component: ProfileComponent
+  },
+  { 
+    path: 'tickets', 
+    component: TicketsComponent
+  },
+  { 
+    path: 'ticket/:id', 
+    component: TicketComponent
+  },
+  { 
+    path: '**', 
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
