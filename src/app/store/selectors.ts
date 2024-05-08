ee47ffa5b934d2b624c848afaa31b372fdfd6f8f
offline2099 <offline2099@protonmail.com>
2024-05-08 20:05:16 +0700

    Create selectors for tickets

--- src/app/store/selectors.ts
+++ src/app/store/selectors.ts
@@ -5,3 +5,11 @@ export const featureUser = (state: AppState) => state.user;
 export const userSelector = createSelector(featureUser, (state) => state.user);
 export const authLoadingSelector = createSelector(featureUser, (state) => state.authLoading);
 export const authErrorSelector = createSelector(featureUser, (state) => state.authError);
+
+export const featureTickets = (state: AppState) => state.tickets;
+export const ticketsSelector = createSelector(featureTickets, (state) => state.tickets);
+export const ticketsLoadingSelector = createSelector(featureTickets, (state) => state.isLoading);
+export const ticketsErrorSelector = createSelector(featureTickets, (state) => state.error);
+export const ticketSelector = (id: number) => createSelector(featureTickets, (state) => 
+  state.tickets.find(t => t.id === id)
+);
