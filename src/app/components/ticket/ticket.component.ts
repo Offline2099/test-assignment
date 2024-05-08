0ad58ab16dbe75e4b7fd1187ccebc89d1c852d3b
offline2099 <offline2099@protonmail.com>
2024-05-08 21:56:33 +0700

    Create page viewing for single ticket

--- src/app/components/ticket/ticket.component.ts
+++ src/app/components/ticket/ticket.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+import { Ticket } from '../../types/ticket.interface';
+import { AppState } from '../../store/types';
+import { ticketSelector } from '../../store/selectors';
 
 @Component({
   selector: 'app-ticket',
@@ -6,5 +12,33 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.scss'
 })
 export class TicketComponent {
+
+  ticketId: number = 0;
+  ticket$: Observable<Ticket | undefined>;
+  sub: Subscription;
+  ticket: Ticket | undefined;
+  ticketSections: {name: string, content: number | string}[] = [];
+
+  constructor(private store: Store<AppState>, private route: ActivatedRoute) {
+    this.ticketId = Number(this.route.snapshot.paramMap.get('id')) || 0;
+    this.ticket$ = this.store.pipe(select(ticketSelector(this.ticketId)));
+    this.sub = this.ticket$.subscribe(t => {
+      this.ticket = t;
+      if (t) this.fillTicketSections(t);
+    });
+  }
+
+  fillTicketSections = (t: Ticket) => {
+    this.ticketSections = [
+      {name: 'Ticket ID', content: t.id},
+      {name: 'Ticket Created', content: t.created},
+      {name: 'Ticket Name', content: t.name},
+      {name: 'Ticket Description', content: t.description}
+    ]
+  } 
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
 }
